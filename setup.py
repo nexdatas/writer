42ff4b4ff63c6f52976d850671cd9a0d2c9e9dbf
jkotan <jankotan@gmail.com>
2012-07-25 10:50:37 +0200

    adding support from distutils

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import os
-from setuptools import setup
+
+__version__="0.0.1"
 
 ## reading a file
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(
+## metadata for distutils
+SETUPDATA=dict(
     name = "nexdatas",
-    version = "0.0.1",
+    version = __version__,
     author = "Jan Kotanski, Eugen Wintersberger , Halil Pasic",
     author_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, halil.pasic@gmail.com",
     description = ("Nexus Data writer implemented as a Tango Server"),
@@ -15,11 +17,31 @@ setup(
     keywords = "writer Tango server nexus data",
     url = "http://code.google.com/p/nexdatas/",
     packages=['ndts'],
+#    package_data={'ndts': ['TDS']},
     long_description= read('README'),
+)
+
+## metadata for setuptools
+SETUPTOOLSDATA= dict(
+    include_package_data = True,
     install_requires = [
         'numpy>=1.5.0',
         'PyTango>=7.2.2',
         'libpninx-python>=0.1.2'
         ],
-
 )
+
+## the main function
+def main():
+    try:
+        import setuptools
+        SETUPDATA.update(SETUPTOOLSDATA)
+        setuptools.setup(**SETUPDATA)
+    except ImportError:
+        import distutils.core
+        distutils.core.setup(**SETUPDATA)
+        
+
+if __name__ == '__main__':
+    main()
+
