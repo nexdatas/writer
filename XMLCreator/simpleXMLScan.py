408a11e8078b5c33655ae67b149d83793076f950
Jan Kotanski <jankotan@gmail.com>
2012-11-23 12:51:25 +0100

    changing tags to 4-spaces

--- XMLCreator/simpleXMLScan.py
+++ XMLCreator/simpleXMLScan.py
@@ -23,55 +23,55 @@ from simpleXML import *
 
 ## the main function
 def main():
-	df = XMLFile("scan.xml")
-	
-	en = NGroup(df, "entry1", "NXentry")
+    df = XMLFile("scan.xml")
+    
+    en = NGroup(df, "entry1", "NXentry")
 
-	## instrument
-	ins = NGroup(en, "instrument", "NXinstrument")
-#	NXsource	
-	dt = NGroup(ins, "detector", "NXdetector")
-	f = NField(dt, "counter1", "NX_FLOAT")
-	f.setUnits("m")
-#	f.setText("0.2")
-	f.setStrategy("STEP")
-	sr = NDSource(f)
+    ## instrument
+    ins = NGroup(en, "instrument", "NXinstrument")
+#    NXsource    
+    dt = NGroup(ins, "detector", "NXdetector")
+    f = NField(dt, "counter1", "NX_FLOAT")
+    f.setUnits("m")
+#    f.setText("0.2")
+    f.setStrategy("STEP")
+    sr = NDSource(f)
 
-	sr.initClient("exp_c01","exp_c01");
-
-
-	f = NField(dt, "counter2", "NX_FLOAT")
-	f.setUnits("s")
-#	f.setText("0.2")
-	f.setStrategy("STEP")
-	sr = NDSource(f)
-	sr.initClient("exp_c02","exp_c02");
-	
-
-	f = NField(dt, "mca", "NX_FLOAT")
-	f.setUnits("")
-	d = NDimensions(f, "1")
-	d.dim("1", "2048")
-	f.setStrategy("STEP")
-	sr = NDSource(f)
-	sr.initTango("p09/mca/exp.02","p09/mca/exp.02", "attribute", "Data")
-#	sr.initClient("p09/mca/exp.02","p09/mca/exp.02")
-
-        ##	NXdata
-	da = NGroup(en, "data", "NXdata")
-	## link
-	l = NLink(da, "data", "/NXentry/NXinstrument/NXdetector/mca")
-	l.addDoc("Link to mca in /NXentry/NXinstrument/NXdetector")
-	l = NLink(da, "counter1", "/NXentry/NXinstrument/NXdetector/counter1")
-	l.addDoc("Link to counter1 in /NXentry/NXinstrument/NXdetector")
-	l = NLink(da, "counter2", "/NXentry/NXinstrument/NXdetector/counter2")
-	l.addDoc("Link to counter2 in /NXentry/NXinstrument/NXdetector")
+    sr.initClient("exp_c01","exp_c01");
 
 
+    f = NField(dt, "counter2", "NX_FLOAT")
+    f.setUnits("s")
+#    f.setText("0.2")
+    f.setStrategy("STEP")
+    sr = NDSource(f)
+    sr.initClient("exp_c02","exp_c02");
+    
 
-	df.dump()
+    f = NField(dt, "mca", "NX_FLOAT")
+    f.setUnits("")
+    d = NDimensions(f, "1")
+    d.dim("1", "2048")
+    f.setStrategy("STEP")
+    sr = NDSource(f)
+    sr.initTango("p09/mca/exp.02","p09/mca/exp.02", "attribute", "Data")
+#    sr.initClient("p09/mca/exp.02","p09/mca/exp.02")
+
+        ##    NXdata
+    da = NGroup(en, "data", "NXdata")
+    ## link
+    l = NLink(da, "data", "/NXentry/NXinstrument/NXdetector/mca")
+    l.addDoc("Link to mca in /NXentry/NXinstrument/NXdetector")
+    l = NLink(da, "counter1", "/NXentry/NXinstrument/NXdetector/counter1")
+    l.addDoc("Link to counter1 in /NXentry/NXinstrument/NXdetector")
+    l = NLink(da, "counter2", "/NXentry/NXinstrument/NXdetector/counter2")
+    l.addDoc("Link to counter2 in /NXentry/NXinstrument/NXdetector")
+
+
+
+    df.dump()
 
 
 
 if __name__ == "__main__":
-	main()
+    main()
