5b0d36fc83514ffea0164d168762227b0f180628
Jan Kotanski <jankotan@gmail.com>
2012-12-17 13:45:43 +0100

    adding tests for client float scalars

--- test/FieldTagWriterTest.py
+++ test/FieldTagWriterTest.py
@@ -43,6 +43,7 @@ class FieldTagWriterTest(unittest.TestCase):
         unittest.TestCase.__init__(self, methodName)
 
         self._counter =  [1,-2,6,-8,9,-11]
+        self._fcounter =  [1.1,-2.4,6.54,-8.456,9.456,-0.46545]
         self._mca1 = [e*0.1 for e in range(2048)]
         self._mca2 = [(float(e)/(100.+e)) for e in range(2048)]
 
@@ -62,7 +63,7 @@ class FieldTagWriterTest(unittest.TestCase):
     # \param f pninx file object    
     # \param fname file name
     # \returns detector group object    
-    def _checkScalarTree(self, f, fname):
+    def _checkScalarTree(self, f, fname, children):
         self.assertEqual("%s/%s" % ( os.getcwd(), f.name), fname)
         self.assertEqual(6, f.nattrs)
         self.assertEqual( f.attr("file_name").value, fname)
@@ -102,7 +103,7 @@ class FieldTagWriterTest(unittest.TestCase):
         self.assertTrue(det.valid)
         self.assertEqual(det.name,"detector")
         self.assertEqual(det.nattrs,1)
-        self.assertEqual(det.nchildren, 10)
+        self.assertEqual(det.nchildren, children)
             
         at = det.attr("NX_class")
         self.assertTrue(at.valid)
@@ -123,7 +124,7 @@ class FieldTagWriterTest(unittest.TestCase):
     # \param dtype numpy type
     # \param nxtype nexus type
     # \param unsigned flag if value is integer
-    def _checkScalarCounter(self, det, name, dtype, nxtype, unsigned = False):
+    def _checkScalarCounter(self, det, name, dtype, nxtype, values, error = 0):
 
         cnt = det.open(name)
         self.assertTrue(cnt.valid)
@@ -134,12 +135,9 @@ class FieldTagWriterTest(unittest.TestCase):
         self.assertEqual(cnt.dtype, dtype)
         self.assertEqual(cnt.size, 6)
         value = cnt.read()
-#            value = cnt[:]
         for i in range(len(value)):
-            if unsigned:
-                self.assertEqual(abs(self._counter[i]), value[i])
-            else:
-                self.assertEqual(self._counter[i], value[i])
+#            print values[i].__repr__(),  value[i].__repr__(), values[i] - value[i]
+            self.assertTrue(abs(values[i]- value[i]) <= error)
                         
         self.assertEqual(cnt.nattrs,3)
 
@@ -170,7 +168,7 @@ class FieldTagWriterTest(unittest.TestCase):
     ## scanRecord test
     # \brief It tests recording of simple h5 file
     def test_clientIntScalar(self):
-        print "Run: FieldTagWriterTest.test_scanRecord() "
+        print "Run: FieldTagWriterTest.test_clientIntScalar() "
         fname= '%s/clientintscalar.h5' % os.getcwd()   
         xml= """<definition>
   <group type="NXentry" name="entry1">
@@ -276,17 +274,87 @@ class FieldTagWriterTest(unittest.TestCase):
         # check the created file
         
         f = open_file(fname,readonly=True)
-        det = self._checkScalarTree(f, fname)
-        self._checkScalarCounter(det, "counter", "int64", "NX_INT")
-        self._checkScalarCounter(det, "counter8", "int8", "NX_INT8")
-        self._checkScalarCounter(det, "counter16", "int16", "NX_INT16")
-        self._checkScalarCounter(det, "counter32", "int32", "NX_INT32")
-        self._checkScalarCounter(det, "counter64", "int64", "NX_INT64")
-        self._checkScalarCounter(det, "ucounter", "uint64", "NX_UINT", True)
-        self._checkScalarCounter(det, "ucounter8", "uint8", "NX_UINT8", True)
-        self._checkScalarCounter(det, "ucounter16", "uint16", "NX_UINT16", True)
-        self._checkScalarCounter(det, "ucounter32", "uint32", "NX_UINT32", True)
-        self._checkScalarCounter(det, "ucounter64", "uint64", "NX_UINT64", True)
+        det = self._checkScalarTree(f, fname , 10)
+        self._checkScalarCounter(det, "counter", "int64", "NX_INT", self._counter)
+        self._checkScalarCounter(det, "counter8", "int8", "NX_INT8", self._counter)
+        self._checkScalarCounter(det, "counter16", "int16", "NX_INT16", self._counter)
+        self._checkScalarCounter(det, "counter32", "int32", "NX_INT32", self._counter)
+        self._checkScalarCounter(det, "counter64", "int64", "NX_INT64", self._counter)
+        self._checkScalarCounter(det, "ucounter", "uint64", "NX_UINT", [abs(c) for c in self._counter])
+        self._checkScalarCounter(det, "ucounter8", "uint8", "NX_UINT8", [abs(c) for c in self._counter]) 
+        self._checkScalarCounter(det, "ucounter16", "uint16", "NX_UINT16", [abs(c) for c in self._counter]) 
+        self._checkScalarCounter(det, "ucounter32", "uint32", "NX_UINT32", [abs(c) for c in self._counter]) 
+        self._checkScalarCounter(det, "ucounter64", "uint64", "NX_UINT64", [abs(c) for c in self._counter]) 
+
+        
+        f.close()
+
+
+
+    ## scanRecord test
+    # \brief It tests recording of simple h5 file
+    def test_clientFloatScalar(self):
+        print "FLOAT"
+        print "Run: FieldTagWriterTest.test_clientFloatScalar() "
+        fname= '%s/clientfloatscalar.h5' % os.getcwd()   
+        xml= """<definition>
+  <group type="NXentry" name="entry1">
+    <group type="NXinstrument" name="instrument">
+      <group type="NXdetector" name="detector">
+        <field units="m" type="NX_FLOAT" name="counter">
+          <strategy mode="STEP"/>
+          <datasource type="CLIENT">
+            <record name="cnt"/>
+          </datasource>
+        </field>
+        <field units="m" type="NX_FLOAT32" name="counter_32">
+          <strategy mode="STEP"/>
+          <datasource type="CLIENT">
+            <record name="cnt_32"/>
+          </datasource>
+        </field>
+        <field units="m" type="NX_FLOAT64" name="counter_64">
+          <strategy mode="STEP"/>
+          <datasource type="CLIENT">
+            <record name="cnt_64"/>
+          </datasource>
+        </field>
+
+      </group>
+    </group>
+  </group>
+</definition>
+"""
+        
+
+        tdw = TangoDataWriter(fname)
+
+        tdw.openNXFile()
+        
+        tdw.xmlSettings = xml
+        
+        tdw.openEntry()
+            
+        for c in self._fcounter:
+            tdw.record('{"data": {"cnt":' + str(c) 
+                       + ', "cnt_32":' + str(c) 
+                       + ', "cnt_64":' + str(c) 
+                       + ' } }')
+        
+        tdw.closeEntry()
+        
+        tdw.closeNXFile()
+            
+
+
+        
+        # check the created file
+        
+        f = open_file(fname,readonly=True)
+        det = self._checkScalarTree(f, fname, 3)
+        self._checkScalarCounter(det, "counter", "float64", "NX_FLOAT", self._fcounter, 1.0e-14)
+        self._checkScalarCounter(det, "counter_64", "float64", "NX_FLOAT64", self._fcounter, 1.0e-14)
+        self._checkScalarCounter(det, "counter_32", "float32", "NX_FLOAT32", self._fcounter, 1.0e-06)
 
         
         f.close()
