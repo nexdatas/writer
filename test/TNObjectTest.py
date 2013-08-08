76fbfcde6019bce251e5820c3238001e0338bdbc
Jan Kotanski <jankotan@gmail.com>
2013-08-08 14:19:59 +0200

    adding test for TNObject consturctor

--- test/TNObjectTest.py
+++ test/TNObjectTest.py
@@ -25,6 +25,7 @@ import sys
 import subprocess
 import random
 import struct
+import binascii
 
 from ndts.FetchNameHandler import TNObject
 
@@ -49,14 +50,17 @@ class ElementTest(unittest.TestCase):
         unittest.TestCase.__init__(self, methodName)
 
 
-        self._tfname = "field"
-        self._tfname = "group"
-        self._fattrs = {"short_name":"test","units":"m" }
-
 
         self._bint = "int64" if IS64BIT else "int32"
         self._buint = "uint64" if IS64BIT else "uint32"
         self._bfloat = "float64" if IS64BIT else "float32"
+        try:
+            self.__seed  = long(binascii.hexlify(os.urandom(16)), 16)
+        except NotImplementedError:
+            self.__seed  = long(time.time() * 256) 
+         
+        self.__rnd = random.Random(self.__seed)
+
 
     ## test starter
     # \brief Common set up
@@ -73,11 +77,68 @@ class ElementTest(unittest.TestCase):
     def test_constructor(self):
         fun = sys._getframe().f_code.co_name
         print "Run: %s.%s() " % (self.__class__.__name__, fun)
-        el = TNObject()
-        self.assertEqual(el.name, 'root')
+        root = TNObject()
+        self.assertEqual(root.name, 'root')
+        self.assertEqual(root.nxtype, None)
+        self.assertEqual(root.parent, None)
+        self.assertEqual(root.children, [])
+
+        nn = self.__rnd.randint(1, 10)
+        mname = "myname%s" % nn
+        mtype = "NXtype%s" % nn
+        el = TNObject(mname)
+        self.assertEqual(el.name, mname)
         self.assertEqual(el.nxtype, None)
         self.assertEqual(el.parent, None)
         self.assertEqual(el.children, [])
+
+        nn = self.__rnd.randint(1, 10)
+        mname = "myname%s" % nn
+        mtype = "NXtype%s" % nn
+        el = TNObject(mname, mtype)
+        self.assertEqual(el.name, mname)
+        self.assertEqual(el.nxtype, mtype)
+        self.assertEqual(el.parent, None)
+        self.assertEqual(el.children, [])
+
+
+        nn = self.__rnd.randint(1, 10)
+        mname = "myname%s" % nn
+        mtype = "NXtype%s" % nn
+        self.assertEqual(root.children, [])
+        el = TNObject(mname, mtype, root)
+        self.assertEqual(root.children, [el])
+        self.assertEqual(el.name, mname)
+        self.assertEqual(el.nxtype, mtype)
+        self.assertEqual(el.parent, root)
+        self.assertEqual(el.children, [])
+
+
+
+        nn = self.__rnd.randint(1, 10)
+        mname = "myname%s" % nn
+        mtype = "NXtype%s" % nn
+        self.assertEqual(root.children, [el])
+        el2 = TNObject(mname, mtype, root)
+        self.assertEqual(root.children, [el, el2])
+        self.assertEqual(el2.name, mname)
+        self.assertEqual(el2.nxtype, mtype)
+        self.assertEqual(el2.parent, root)
+        self.assertEqual(el2.children, [])
+
+
+        nn = self.__rnd.randint(1, 10)
+        mname = "myname%s" % nn
+        mtype = "NXtype%s" % nn
+        self.assertEqual(root.children, [el,el2])
+        self.assertEqual(el2.children, [])
+        el3 = TNObject(mname, mtype, el2)
+        self.assertEqual(el2.children, [el3])
+        self.assertEqual(root.children, [el, el2])
+        self.assertEqual(el3.name, mname)
+        self.assertEqual(el3.nxtype, mtype)
+        self.assertEqual(el3.parent, el2)
+        self.assertEqual(el3.children, [])
 
 
         
