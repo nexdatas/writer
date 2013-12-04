a2851a5ac5c2a368e969dc3f5ecf1f947ee07076
Jan Kotanski <jkotan@wheezy-jk.desy.de>
2013-12-04 13:54:43 +0100

    cleaning setup.py

--- setup.py
+++ setup.py
@@ -17,9 +17,12 @@
 #    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
 ## \file setup.py
 # nxswriter installer 
-import os,sys
+
+""" setup.py for Nexus Data Writer """
+
+
+import os
 from distutils.core import setup
-from distutils.command.install import install
 
 ## package name
 NDTS = "nxswriter"
@@ -45,11 +48,12 @@ required = [
     ]
 
 ## metadata for distutils
-SETUPDATA=dict(
+SETUPDATA = dict(
     name = "nexdatas",
     version = INDTS.__version__,
     author = "Jan Kotanski, Eugen Wintersberger , Halil Pasic",
-    author_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, halil.pasic@gmail.com",
+    author_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, " \
+        + "halil.pasic@gmail.com",
     description = ("Nexus Data writer implemented as a Tango Server"),
     license = "GNU GENERAL PUBLIC LICENSE v3",
     keywords = "writer Tango server nexus data",
