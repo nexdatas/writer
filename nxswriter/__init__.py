8979234ee177e83e872e0e903baedc7562aba72b
Jan Kotanski <jankotan@gmail.com>
2017-06-23 13:32:27 +0200

    move globallock into __init__

--- nxswriter/__init__.py
+++ nxswriter/__init__.py
@@ -19,12 +19,16 @@
 
 """ Tango Data Writer """
 
+import threading
+
 #: package version
 from .Release import __version__
 from .TangoDataWriter import globallock
 
-__all__ = ["__version__", "run", "globallock"]
+#: global lock
+globallock = threading.Lock()
 
+__all__ = ["__version__", "run", "globallock"]
 
 def run(argv):
     """ launches the TANGO server
