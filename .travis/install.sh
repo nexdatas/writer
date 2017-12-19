7b4bbad5283a9050fcf0529f9c43847ed8f4e790
Jan Kotanski <jankotan@gmail.com>
2017-12-19 11:32:47 +0100

    back to deprecated-2017Q4

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,13 +14,13 @@ else
     docker exec -it --user root ndts service mysql restart
 fi
 
-docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y --force-yes  tango-db tango-common; sleep 10'
+docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  tango-db tango-common; sleep 10'
 if [ $? -ne "0" ]
 then
     exit -1
 fi
 echo "install tango servers"
-docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y --force-yes tango-starter tango-test liblog4j1.2-java'
+docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java'
 if [ $? -ne "0" ]
 then
     exit -1
