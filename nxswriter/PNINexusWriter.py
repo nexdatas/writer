79da6f2c53ee8771bc67d7ea6dac57b8cdbb4734
Jan Kotanski <jankotan@gmail.com>
2018-06-13 16:24:37 +0200

    add PNINexusWriter

--- nxswriter/PNINexusWriter.py
+++ nxswriter/PNINexusWriter.py
@@ -19,12 +19,108 @@
 
 """ Provides pni file writer """
 
+import math
 from pninexus import h5cpp
 from pninexus import nexus
 
 # import pni.io.nx.h5 as nx
 
 from . import FileWriter
+
+
+def _slice2selection(t):
+    """ converts slice(s) to selection
+
+    :param t: slice tuple
+    :type t: :obj:`tuple`
+    :returns: hyperslab selection
+    :rtype: :class:`h5cpp.dataspace.Hyperslab`
+    """
+
+    if t is Ellipsis:
+        return None
+    elif isinstance(t, slice):
+        if t.step in [None, 1]:
+            return h5cpp.dataspace.Hyperslab(
+                offset=(t.start,),
+                block=(t.stop-t.start,))
+        else:
+            return h5cpp.dataspace.Hyperslab(
+                offset=(t.start,),
+                count=int(math.ceil((t.stop-t.start)/float(t.step))),
+                stride=(t.step - 1,))
+    elif isinstance(t, int):
+        return h5cpp.dataspace.Hyperslab(
+            offset=(t,), block=(1,))
+    elif isinstance(t, list):
+        offset = []
+        block = []
+        count = []
+        stride = []
+        for tel in t:
+            if isinstance(tel, int):
+                offset.append(tel)
+                block.append(1)
+                count.append(1)
+                stride.append(0)
+            elif isinstance(t, slice):
+                if t.step in [None, 1]:
+                    offset.append(tel.start)
+                    block.append(tel.stop - tel.start)
+                    count.append(1)
+                    stride.append(0)
+                else:
+                    offset.append(tel.start)
+                    block.append(1)
+                    count.append(
+                        int(math.ceil(
+                            (tel.stop - tel.start) / float(tel.step))))
+                    stride.append(tel.step - 1)
+        if len(offset):
+            return h5cpp.dataspace.Hyperslab(
+                offset=offset, block=block, count=count, stride=stride)
+
+
+pTh = {
+    "long": h5cpp.datatype.Integer,
+    "str": h5cpp.datatype.kVariableString,
+    "unicode": h5cpp.datatype.kVariableString,
+    "bool": h5cpp.datatype.Integer,
+    "int": h5cpp.datatype.Integer,
+    "int64": h5cpp.datatype.kInt64,
+    "int32": h5cpp.datatype.kInt32,
+    "int16": h5cpp.datatype.kIn16,
+    "int8": h5cpp.datatype.kInt8,
+    "uint": h5cpp.datatype.kInt64,
+    "uint64": h5cpp.datatype.kUInt64,
+    "uint32": h5cpp.datatype.kUInt32,
+    "uint16": h5cpp.datatype.kUInt16,
+    "uint8": h5cpp.datatype.kUInt8,
+    "float": h5cpp.datatype.Float,
+    "float64": h5cpp.datatype.Float64,
+    "float32": h5cpp.datatype.Float32,
+    "float16": h5cpp.datatype.Float16,
+    "string": h5cpp.datatype.kVariableString
+}
+
+
+hTp = {
+    h5cpp.datatype.Integer: "long",
+    h5cpp.datatype.kVariableString: "string",
+    h5cpp.datatype.kInt64: "int64",
+    h5cpp.datatype.kInt32: "int32",
+    h5cpp.datatype.kIn16: "int16",
+    h5cpp.datatype.kInt8: "int8",
+    h5cpp.datatype.kInt64: "uint",
+    h5cpp.datatype.kUInt64: "uint64",
+    h5cpp.datatype.kUInt32: "uint32",
+    h5cpp.datatype.kUInt16: "uint16",
+    h5cpp.datatype.kUInt8: "uint8",
+    h5cpp.datatype.Float:  "float",
+    h5cpp.datatype.Float64: "float64",
+    h5cpp.datatype.Float32: "float32",
+    h5cpp.datatype.Float16: "float16",
+}
 
 
 def open_file(filename, readonly=False, libver=None):
@@ -41,13 +137,14 @@ def open_file(filename, readonly=False, libver=None):
     """
     fapl = h5cpp.property.FileAccessList()
     flag = h5cpp.file.AccessFlags.READONLY if readonly \
-           else h5cpp.file.AccessFlags.READWRITE
-    if libver is None or libver=='lastest':
+        else h5cpp.file.AccessFlags.READWRITE
+    if libver is None or libver == 'lastest':
         fapl.library_version_bounds(
             h5cpp.property.LibVersion.LATEST,
             h5cpp.property.LibVersion.LATEST)
 
     return PNINexusFile(h5cpp.file.open(filename, flag, fapl), filename)
+
 
 def create_file(filename, overwrite=False, libver=None):
     """ create a new file
@@ -64,8 +161,8 @@ def create_file(filename, overwrite=False, libver=None):
     fcpl = h5cpp.property.FileCreationList()
     fapl = h5cpp.property.FileAccessList()
     flag = h5cpp.file.AccessFlags.TRUNCATE if overwrite \
-           else h5cpp.file.AccessFlags.EXCLUSIVE
-    if libver is None or libver=='lastest':
+        else h5cpp.file.AccessFlags.EXCLUSIVE
+    if libver is None or libver == 'lastest':
         fapl.library_version_bounds(
             h5cpp.property.LibVersion.LATEST,
             h5cpp.property.LibVersion.LATEST)
@@ -183,19 +280,18 @@ class PNINexusFile(FileWriter.FTFile):
         """
 
         fapl = h5cpp.property.FileAccessList()
-        if libver is None or libver=='lastest' or swmr:
+        if libver is None or libver == 'lastest' or swmr:
             fapl.library_version_bounds(
                 h5cpp.property.LibVersion.LATEST,
                 h5cpp.property.LibVersion.LATEST)
 
-        if  smwr:
+        if swmr:
             flag = h5cpp.file.AccessFlags.READWRITE \
-                   | h5cpp.file.AccessFlags.SWMRWRITE
+                | h5cpp.file.AccessFlags.SWMRWRITE
         elif readonly:
             flag = h5cpp.file.AccessFlags.READONLY
         else:
             flag = h5cpp.file.AccessFlags.READWRITE
-            
 
         self._h5object = nexus.open_file(self.name, flag, fapl)
         FileWriter.FTFile.reopen(self)
@@ -282,9 +378,13 @@ class PNINexusGroup(FileWriter.FTGroup):
         dcpl = h5cpp.property.DatasetCreationList()
         if dfilter:
             dfilter.h5object(dcpl)
+            if dfilter.shuffle:
+                sfilter = h5cpp.filter.Shuffle()
+                sfilter(dcpl)
         return PNINexusField(
             nexus.FieldFactory.create(
-            self._h5object, name, type_code, shape, chunk, dcpl=dcpl),
+                self._h5object, name, type_code, shape,
+                chunk=chunk, dcpl=dcpl),
             self
         )
 
@@ -318,8 +418,8 @@ class PNINexusGroup(FileWriter.FTGroup):
         if isinstance(self._tparent, PNINexusFile):
             self._h5object = self._tparent.h5object.root()
         else:
-             self._h5object = nexus.get_objects(
-                 self._tparent.h5object, nexus.Path(h5cpp.Path(self.name)))[0]
+            self._h5object = nexus.get_objects(
+                self._tparent.h5object, nexus.Path(h5cpp.Path(self.name)))[0]
         FileWriter.FTGroup.reopen(self)
 
     def exists(self, name):
@@ -412,9 +512,9 @@ class PNINexusField(FileWriter.FTField):
         #: (:obj:`str`) object name
         self.name = None
         if hasattr(h5object, "path"):
-            self.path = h5object.path
+            self.path = h5object.link.path
         if hasattr(h5object, "name"):
-            self.name = h5object.name
+            self.name = str(h5object.link.path).split("/")[-1]
 
     @property
     def attributes(self):
@@ -434,7 +534,8 @@ class PNINexusField(FileWriter.FTField):
     def reopen(self):
         """ reopen field
         """
-        self._h5object = self._tparent.h5object.open(self.name)
+        self._h5object = nexus.get_objects(
+            self._tparent.h5object, nexus.Path(h5cpp.Path(self.name)))[0]
         FileWriter.FTField.reopen(self)
 
     def refresh(self):
@@ -443,7 +544,8 @@ class PNINexusField(FileWriter.FTField):
         :returns: refreshed
         :rtype: :obj:`bool`
         """
-        return False
+        self._h5object.refresh()
+        return True
 
     def grow(self, dim=0, ext=1):
         """ grow the field
@@ -453,7 +555,7 @@ class PNINexusField(FileWriter.FTField):
         :param dim: size of the grow
         :type dim: :obj:`int`
         """
-        self._h5object.grow(dim, ext)
+        self._h5object.extent(dim, ext)
 
     def read(self):
         """ read the field value
@@ -479,7 +581,12 @@ class PNINexusField(FileWriter.FTField):
         :param o: pni object
         :type o: :obj:`any`
         """
+        selection = _slice2selection(t)
         self._h5object.__setitem__(t, o)
+        if selection is None:
+            self._h5object.write(o)
+        else:
+            self._h5object.write(o, selection=selection)
 
     def __getitem__(self, t):
         """ get value
@@ -489,7 +596,11 @@ class PNINexusField(FileWriter.FTField):
         :returns: pni object
         :rtype: :obj:`any`
         """
-        return self._h5object.__getitem__(t)
+        selection = _slice2selection(t)
+        if selection is None:
+            return self._h5object.read()
+        else:
+            return self._h5object.read(selection=selection)
 
     @property
     def is_valid(self):
@@ -507,7 +618,7 @@ class PNINexusField(FileWriter.FTField):
         :returns: field data type
         :rtype: :obj:`str`
         """
-        return self._h5object.dtype
+        return hTp[self._h5object.datatype.type]
 
     @property
     def shape(self):
@@ -516,7 +627,7 @@ class PNINexusField(FileWriter.FTField):
         :returns: field shape
         :rtype: :obj:`list` < :obj:`int` >
         """
-        return self._h5object.shape
+        return self._h5object.dataspace.current_dimensions
 
     @property
     def size(self):
@@ -525,7 +636,7 @@ class PNINexusField(FileWriter.FTField):
         :returns: field size
         :rtype: :obj:`int`
         """
-        return self._h5object.size
+        return self._h5object.dataspace.size
 
 
 class PNINexusLink(FileWriter.FTLink):
@@ -549,7 +660,7 @@ class PNINexusLink(FileWriter.FTLink):
         if hasattr(h5object, "path"):
             self.path = h5object.path
         if hasattr(h5object, "name"):
-            self.name = h5object.name
+            self.name = str(h5object.path).split("/")[-1]
 
     @property
     def is_valid(self):
@@ -566,7 +677,22 @@ class PNINexusLink(FileWriter.FTLink):
         :returns: refreshed
         :rtype: :obj:`bool`
         """
-        return False
+        self._h5object.refresh()
+        return True
+
+    @classmethod
+    def getfilename(cls, obj):
+        filename = ""
+        while not filename:
+            par = obj.parent
+            if par is None:
+                break
+            if isinstance(par, PNINexusFile):
+                filename = par.name
+                break
+            else:
+                obj = par
+        return filename
 
     @property
     def target_path(self):
@@ -575,14 +701,19 @@ class PNINexusLink(FileWriter.FTLink):
         :returns: target path
         :rtype: :obj:`str`
         """
-        return str(self._h5object.target_path)
+        fpath = self._h5object.target().file_path
+        opath = self._h5object.target().object_path
+        if not fpath:
+            fpath = self.getfilename(self)
+        return "%s:/%s" % (fpath, opath)
 
     def reopen(self):
         """ reopen field
         """
-        lks = nx.get_links(self._tparent.h5object)
+        lks = self._tparent.h5object.links
         try:
-            lk = [e for e in lks if e.name == self.name][0]
+            lk = [e for e in lks
+                  if str(e.path).split("/")[-1] == self.name][0]
             self._h5object = lk
         except:
             self._h5object = None
@@ -607,6 +738,7 @@ class PNINexusDeflate(FileWriter.FTDeflate):
         :type h5object: :obj:`any`
         """
         FileWriter.FTDeflate.__init__(self, h5object)
+        self.__shuffle = None
 
     def __getrate(self):
         """ getter for compression rate
@@ -614,7 +746,7 @@ class PNINexusDeflate(FileWriter.FTDeflate):
         :returns: compression rate
         :rtype: :obj:`int`
         """
-        return self._h5object.rate
+        return self._h5object.level
 
     def __setrate(self, value):
         """ setter for compression rate
@@ -622,7 +754,7 @@ class PNINexusDeflate(FileWriter.FTDeflate):
         :param value: compression rate
         :type value: :obj:`int`
         """
-        self._h5object.rate = value
+        self._h5object.level = value
 
     #: (:obj:`int`) compression rate
     rate = property(__getrate, __setrate)
@@ -633,7 +765,7 @@ class PNINexusDeflate(FileWriter.FTDeflate):
         :returns: compression shuffle
         :rtype: :obj:`bool`
         """
-        return self._h5object.shuffle
+        return self.__shuffle
 
     def __setshuffle(self, value):
         """ setter for compression shuffle
@@ -641,7 +773,7 @@ class PNINexusDeflate(FileWriter.FTDeflate):
         :param value: compression shuffle
         :type value: :obj:`bool`
         """
-        self._h5object.shuffle = value
+        self.__shuffle = value
 
     #: (:obj:`bool`) compression shuffle
     shuffle = property(__getshuffle, __setshuffle)
@@ -662,13 +794,9 @@ class PNINexusAttributeManager(FileWriter.FTAttributeManager):
         """
         FileWriter.FTAttributeManager.__init__(self, h5object, tparent)
         #: (:obj:`str`) object nexus path
-        self.path = None
+        self.path = ''
         #: (:obj:`str`) object name
         self.name = None
-        if hasattr(h5object, "path"):
-            self.path = h5object.path
-        if hasattr(h5object, "name"):
-            self.name = h5object.name
 
     def create(self, name, dtype, shape=None, overwrite=False):
         """ create a new attribute
@@ -684,10 +812,21 @@ class PNINexusAttributeManager(FileWriter.FTAttributeManager):
         :returns: attribute object
         :rtype: :class:`PNINexusAtribute`
         """
+        names = [at.name for at in self._h5object.attributes]
+        if name in names:
+            if overwrite:
+                self._h5object.remove(name)
+            else:
+                raise Exception("Attribute %s exists" % name)
         shape = shape or []
-        return PNINexusAttribute(
-            self._h5object.create(
-                name, dtype, shape, overwrite), self.parent)
+        if shape:
+            return PNINexusAttribute(
+                self._h5object.create(
+                    name, pTh[dtype], shape), self.parent)
+        else:
+            return PNINexusAttribute(
+                self._h5object.create(
+                    name, pTh[dtype]), self.parent)
 
     def __len__(self):
         """ number of attributes
@@ -726,7 +865,7 @@ class PNINexusAttributeManager(FileWriter.FTAttributeManager):
         :returns: valid flag
         :rtype: :obj:`bool`
         """
-        return self.parent.is_valid
+        return True
 
 
 class PNINexusAttribute(FileWriter.FTAttribute):
@@ -810,7 +949,7 @@ class PNINexusAttribute(FileWriter.FTAttribute):
         :returns: attribute data type
         :rtype: :obj:`str`
         """
-        return self._h5object.dtype
+        return hTp[self._h5object.datatype.type]
 
     @property
     def shape(self):
@@ -819,7 +958,10 @@ class PNINexusAttribute(FileWriter.FTAttribute):
         :returns: attribute shape
         :rtype: :obj:`list` < :obj:`int` >
         """
-        return self._h5object.shape
+        if hasattr(self._h5object.dataspace, "current_dimensions"):
+            return self._h5object.dataspace.current_dimensions
+        else:
+            []
 
     def reopen(self):
         """ reopen attribute
