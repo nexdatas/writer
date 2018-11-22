4a3934e4743b7ef705cccfdbb61d2b2e61ffec17
Jan Kotanski <jankotan@gmail.com>
2018-11-22 15:01:17 +0100

    improve test server teardown

--- test/SimpleServerSetUp.py
+++ test/SimpleServerSetUp.py
@@ -124,21 +124,35 @@ class SimpleServerSetUp(object):
         print("tearing down ...")
         db = PyTango.Database()
         db.delete_server(self.new_device_info_writer.server)
-
-        pipe = subprocess.Popen(
-            "ps -ef | grep 'SimpleServer.py %s'" % self.instance,
-            stdout=subprocess.PIPE, shell=True).stdout
-
         if sys.version_info > (3,):
-            res = str(pipe.read(), "utf8").split("\n")
+            with subprocess.Popen(
+                    "ps -ef | grep 'SimpleServer.py %s' | grep -v grep" %
+                    self.instance,
+                    stdout=subprocess.PIPE, shell=True) as proc:
+
+                pipe = proc.stdout
+                res = str(pipe.read(), "utf8").split("\n")
+                for r in res:
+                    sr = r.split()
+                    if len(sr) > 2:
+                        subprocess.call(
+                            "kill -9 %s" % sr[1], stderr=subprocess.PIPE,
+                            shell=True)
+                pipe.close()
         else:
+            pipe = subprocess.Popen(
+                "ps -ef | grep 'SimpleServer.py %s' | grep -v grep" %
+                self.instance,
+                stdout=subprocess.PIPE, shell=True).stdout
+
             res = str(pipe.read()).split("\n")
-        for r in res:
-            sr = r.split()
-            if len(sr) > 2:
-                subprocess.call("kill -9 %s" %
-                                sr[1], stderr=subprocess.PIPE, shell=True)
-        pipe.close()
+            for r in res:
+                sr = r.split()
+                if len(sr) > 2:
+                    subprocess.call(
+                        "kill -9 %s" % sr[1], stderr=subprocess.PIPE,
+                        shell=True)
+            pipe.close()
 
 
 if __name__ == "__main__":
