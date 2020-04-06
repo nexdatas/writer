9737f63af49c244608a3c28223c6d0714cd61eff
Jan Kotanski <jankotan@gmail.com>
2020-04-06 13:01:01 +0200

    install h5py by hand for focal

--- .travis/install.sh
+++ .travis/install.sh
@@ -39,6 +39,9 @@ else
     echo "install python3-pytango"
     if [ $1 = "ubuntu20.04" ]; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python3-tango python3-tz; apt-get -qq install -y nxsconfigserver-db python3-nxstools python3-setuptools; sleep 10'
+	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/h5py/h5py h5py'
+	docker exec -it --user root ndts /bin/sh -c 'cd h5py; python3 setup.py install'
+
     else
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python3-pytango python3-tz; apt-get -qq install -y nxsconfigserver-db python3-nxstools python3-setuptools; sleep 10'
     fi
